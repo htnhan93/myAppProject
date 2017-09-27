7d9243920f5f8d1214ca23b31d0ed50ce90a908c
htnhan93 <htnhan93@gmail.com>
2017-09-27 09:04:12 +0700

    create project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  tableData = [
+    { 'drugId': 'CHI0001', 'drugName': 'Chi..... 1', 'uom': 'Viên', 'lotNo': '123456', 'inventory': '15', 'actually': '15' },
+    { 'drugId': 'CHI0002', 'drugName': 'Chi..... 2', 'uom': 'Viên', 'lotNo': '123457', 'inventory': '150', 'actually': '150' },
+    { 'drugId': 'CHI0003', 'drugName': 'Chi..... 3', 'uom': 'Viên', 'lotNo': '123458', 'inventory': '1500', 'actually': '1500' },
+    { 'drugId': 'CHI0004', 'drugName': 'Chi..... 4', 'uom': 'Viên', 'lotNo': '123459', 'inventory': '150', 'actually': '150' },
+    { 'drugId': 'CHI0005', 'drugName': 'Chi..... 5', 'uom': 'Viên', 'lotNo': '123450', 'inventory': '15', 'actually': '15' }
+  ]
+  constructor() {
+
+  }
 }
